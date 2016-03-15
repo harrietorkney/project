f267fb5062e284f38cfd55621bd4f6de79bb42db
harrietorkney <hrorkney1@sheffield.ac.uk>
2016-03-15 16:15:59 +0000

    Changes 15.03

---
+++ map.js
@@ -0,0 +1,8 @@
+/*! Funky Google Maps scripts
+
+-- Latitude/Longitude for addresses are:
+
+205-207 Crookes Valley Rd, Sheffield, South Yorkshire S10 1BA	- (53.3851604, -1.4936113)
+347 Glossop Rd, Sheffield S10 2HP - (53.378757, -1.489900)
+31 Chesterfield Rd, Sheffield S8 0RL - (53.358669, -1.475358)
+*/
